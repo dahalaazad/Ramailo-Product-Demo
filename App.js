62350ffb07bf7cf9fb437f2afcf41a8993d4a3ae
dahalaazad <aazad.dahal@gmail.com>
2023-08-24 22:40:14 +0545

    feat: Change screen header title

--- App.js
+++ App.js
@@ -2,19 +2,27 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {DetailScreen, HomeScreen} from './src/screens';
+import {useSelector} from 'react-redux';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const currentProduct = useSelector(state => state?.currentProduct);
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator
-        // screenOptions={{headerShown: false}}
-        >
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{headerTitle: 'All Products'}}
+          />
 
-          <Stack.Screen name="DetailScreen" component={DetailScreen} />
+          <Stack.Screen
+            name="DetailScreen"
+            component={DetailScreen}
+            options={{headerTitle: currentProduct?.title}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
