95ded368a20782b04d83adb58b2dff9528797620
dahalaazad <aazad.dahal@gmail.com>
2023-08-25 00:20:11 +0545

    feat: Complete DetailScreen

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,9 +1,68 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import Gallery from 'react-native-awesome-gallery';
+import {heightToDp, widthToDp} from '../utils';
 
 const DetailScreen = () => {
+  const currentProduct = useSelector(state => state?.currentProduct);
   return (
     <View style={styles.container}>
-      <Text>DetailScreen</Text>
+      <View style={{paddingHorizontal: widthToDp(20)}}>
+        <Text
+          style={{fontSize: 24, color: '#fff', paddingBottom: heightToDp(0)}}>
+          {currentProduct?.title}
+        </Text>
+
+        <Text
+          style={{fontSize: 16, color: '#fff', paddingBottom: heightToDp(10)}}>
+          {currentProduct?.brand}
+        </Text>
+
+        <Text
+          style={{fontSize: 18, color: '#fff', paddingBottom: heightToDp(10),fontWeight:'700' }}>
+          ${currentProduct?.price}
+        </Text>
+
+        <Text
+          style={{fontSize: 16, color: '#fff', paddingBottom: heightToDp(10)}}>
+          Discount Available: {currentProduct?.discountPercentage.toFixed(0)}%
+        </Text>
+
+        <Text style={{fontSize: widthToDp(16), paddingBottom: heightToDp(10)}}>
+          {currentProduct?.description}
+        </Text>
+
+        <Text
+          style={{fontSize: 16, color: '#fff', paddingBottom: heightToDp(10)}}>
+          Rating: {currentProduct?.rating.toFixed(1)}{' '}
+        </Text>
+      </View>
+
+      <View
+        style={{
+          paddingHorizontal: widthToDp(20),
+          borderRadius: widthToDp(20),
+          paddingTop: heightToDp(50),
+        }}>
+        <FlatList
+          data={currentProduct?.images}
+          horizontal
+          renderItem={({item}) => (
+            <Image
+              source={{uri: item}}
+              resizeMode="contain"
+              width={widthToDp(200)}
+              height={widthToDp(200)}
+              alt="Thumbnail"
+              style={{borderRadius: widthToDp(25)}}
+            />
+          )}
+          ItemSeparatorComponent={
+            <View style={{paddingHorizontal: widthToDp(10)}} />
+          }
+          keyExtractor={item => item.id}
+        />
+      </View>
     </View>
   );
 };
@@ -14,7 +73,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: widthToDp(20),
+    paddingTop: heightToDp(30),
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
