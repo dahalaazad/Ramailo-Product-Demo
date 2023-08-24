2556bbeaa56e2052e2dcb3f9254e42655b8f1866
dahalaazad <aazad.dahal@gmail.com>
2023-08-24 22:53:06 +0545

    feat: Complete ProductCard

--- src/components/atoms/ProductCard.js
+++ src/components/atoms/ProductCard.js
@@ -1,12 +1,49 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {heightToDp, widthToDp} from '../../utils';
 import {Colors} from '../../constants';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const ProductCard = ({productDetails}) => {
+const ProductCard = ({productDetails, onPressHandler}) => {
+  const shortenString = str => {
+    if (str.length > 22) {
+      return str.substring(0, 19) + '...';
+    }
+    return str;
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.playIconContainer}>
-        <Text style={{color:'#000'}} >{productDetails?.title} </Text>
+      <TouchableOpacity
+        style={styles.playIconContainer}
+        onPress={onPressHandler}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{paddingRight: widthToDp(10)}}>
+            <Image
+              source={{uri: productDetails?.images[0]}}
+              resizeMode="cover"
+              height={50}
+              width={50}
+              alt="Thumbnail"
+              style={{borderRadius: widthToDp(25)}}
+            />
+          </View>
+
+          <View style={{}}>
+            <Text style={styles.textStyle}>
+              {shortenString(productDetails?.title)}
+            </Text>
+
+            <Text style={[styles.textStyle, {color: '#2888F8'}]}>
+              {productDetails?.category}
+            </Text>
+
+            <Text style={[styles.textStyle]}>${productDetails?.price}</Text>
+          </View>
+        </View>
+
+        <View style={{justifyContent: 'center'}}>
+          <Ionicons name="chevron-forward" size={widthToDp(30)} color="#000" />
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -17,7 +54,6 @@ export default ProductCard;
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    marginHorizontal: widthToDp(30),
     marginVertical: heightToDp(8),
     borderRadius: widthToDp(20),
     shadowRadius: 20,
@@ -25,12 +61,20 @@ const styles = StyleSheet.create({
   },
   playIconContainer: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     backgroundColor: Colors.workoutCardRedColor,
-    paddingVertical: heightToDp(30),
+    paddingVertical: heightToDp(20),
     paddingHorizontal: widthToDp(12),
     borderRadius: widthToDp(20),
     elevation: 1,
     shadowRadius: 20,
     shadowColor: Colors.boxShadowColor,
+  },
+  textStyle: {
+    color: '#000',
+    fontSize: widthToDp(16),
+    fontWeight: '600',
+    paddingHorizontal: widthToDp(30),
   },
 });
